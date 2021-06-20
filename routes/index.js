62f1ed8b867bd7934ad9342aa5739248dcaf0af5
Jeff Follestad <jeffrey.follestad@gmail.com>
2021-06-20 10:42:26 -0700

    updated index.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
-const categoryRoutes = require('./category-routes');
-const productRoutes = require('./product-routes');
-const tagRoutes = require('./tag-routes');
 
-router.use('/categories', categoryRoutes);
-router.use('/products', productRoutes);
-router.use('/tags', tagRoutes);
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
 
 module.exports = router;
