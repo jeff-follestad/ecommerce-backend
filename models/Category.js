3ca7feb7709eae9c4a6fa5aad5c6c363d3b5ff20
Jeff Follestad <jeffrey.follestad@gmail.com>
2021-06-19 18:17:40 -0700

    built Category.js model

--- models/Category.js
+++ models/Category.js
@@ -4,9 +4,17 @@ const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
+//create fields/columns for Category model
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: DataTypes.STRING,
+    allowNull: false
   },
   {
     sequelize,
